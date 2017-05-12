e923e1db53b1a72eea1818af7ae230ccc54f220b
remolalata <lalata.remo@gmail.com>
2017-05-12 10:35:59 +0800

    edited by remo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  $('.flex_grid').flexImages({ rowHeight: 320, maxRows: 29, truncate: true });
+  $('.flex_grid.credits').flexImages({ rowHeight: 320, maxRows: 29, truncate: true });
+  $('.flex_grid.sponsored').flexImages({rowHeight: 160, maxRows: 1, truncate: true });
+  $('.flex_grid.search-baby').flexImages({rowHeight: 300, maxRows: 5});
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.flex_grid').flexImages({ rowHeight: 320, maxRows: 29, truncate: true });
+  })
 });
